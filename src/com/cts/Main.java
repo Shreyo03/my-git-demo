67fb11289415ce76279b44f58f68cc26522e87cd
Shreyo03 <shreyoshreekorangae3@gmail.com>
2022-05-30 13:59:59 +0530

    initial branch 'issu1' commit

--- src/com/cts/Main.java
+++ src/com/cts/Main.java
@@ -1,11 +1,16 @@
 package com.cts;
 
+import com.cts.service.SomeService;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Welcome");
 System.out.println("Hello");
+
+SomeService ss=new SomeService();
+ System.out.println(ss.findLength("Program"));
 	}
 
 }
